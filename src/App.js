db9f3dabb8fc9b0e932e9e7abd20fef6ddf85c60
Ameerdeen N <ameerdeen592@gmail.com>
2024-09-28 17:06:23 +0530

    success

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Products from './Components/Products';
 import CartPage from './Components/CartPage';
 
