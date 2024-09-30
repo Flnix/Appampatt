7882bd9d9ea54bfb7436a531ebb02eeeebb206bf
Ameerdeen N <ameerdeen592@gmail.com>
2024-09-30 13:28:13 +0530

    success

---
+++ src/Components/ContactCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Stylings/ContactCard.css'
+
+export default function ContactCard() {
+  return (
+    <div className='colour-warning'>
+      <p>Mobile : +91 6369715501</p>
+      <p>Address :Opposite to Gemgrow Appartment, Wings Sports academy road Padur, 627117</p>
+      <p>Email :jkasnjdna</p>
+    </div>
+  )
+}
